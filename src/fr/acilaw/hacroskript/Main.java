66e3823c2a50aaf4415a07a24a6a835009c4a407
Acilaw06 <alexandre.benoit.06@gmail.com>
2024-04-24 21:05:58 +0200

    test

---
+++ src/fr/acilaw/hacroskript/Main.java
@@ -0,0 +1,16 @@
+package fr.acilaw.hacroskript;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("§8[§eHacro§6Skript§8] §a Le plugin vient de démarrer !");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("§8[§eHacro§6Skript§8] §c Le plugin vient s'eteindre !");
+    }
+}
